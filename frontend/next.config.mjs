09238848aa833f87aa9d9daf6a801431b60fad3c
balsemkhouni <balsem.khouni@esprit.tn>
2026-06-23 14:38:03 +0100

    fix vercel config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,25 +1,15 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push("canvas");
     }
     return config;
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3001/:path*",
-      },
-    ];
-  },
+  }
 };
 
 export default withNextIntl(nextConfig);
